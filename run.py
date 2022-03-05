3fed7c32b4ef819fe12e5d371173eba89abcfa75
Yassen Shopov <yassenshopov00@gmail.com>
2022-03-05 16:09:44 +0000

    Removed old Flask files

--- run.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-
-@app.route('/')
-def current_template():
-   return render_template('current/index.html')
-
-if __name__ == '__main__':
-   app.run()
